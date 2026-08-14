789413b47ec7bbad821dea2b9dbe2c49258c72f8
zlc-0330 <zlc-0330@users.noreply.github.com>
2026-08-14 05:56:20 +0000

    Update cesium-example site zlc-0330/cesium-example@74c381d8ca8d8d454a636b0b683aab376617fbef

--- cesium-example/config.js
+++ cesium-example/config.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: zhanglichun 954751293@qq.com
+ * @Date: 2026-07-08 09:27:53
+ * @LastEditors: zhanglichun 954751293@qq.com
+ * @LastEditTime: 2026-08-04 10:00:21
+ * @FilePath: \cesium-example\public\config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // 唯一配置源头，只在浏览器运行
 const BASE_URL = "project/cesium-example"
+// const SOURCE_BASE = 'https://cdn.jsdelivr.net/gh/zlc-0330/project-file-server@latest/cesiumExample/'
 const SOURCE_BASE = 'https://zlc-0330.github.io/project-file-server/cesiumExample/'
 
 const config = {
